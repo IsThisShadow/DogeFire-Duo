8d5319ff229851217c978a686de50ea2cfb87c84
logandav3707 <ldavis@ftstudent.org>
2025-02-13 13:16:56 -0500

    g

--- TestScene/scripts/enemy.gd
+++ TestScene/scripts/enemy.gd
@@ -36,7 +36,3 @@ func _set_random_vertical_direction() -> void:
 func wavy_movement(delta, frequency: float, amplitude: float) -> void:
 	time -= frequency * delta
 	position.y = pivot_position.y + sin(time) * amplitude
-
-# Remove the ship when it leaves the screen
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
