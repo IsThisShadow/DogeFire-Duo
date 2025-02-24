02c6e1029bb645af20c2ee77f02c9313f46f4dfa
6B47 <jerrylin1144@gmail.com>
2025-02-24 12:38:47 -0500

    Weapon System
    Shooting and bullet.

--- scripts/PlayerScripts/Player.gd
+++ scripts/PlayerScripts/Player.gd
@@ -5,12 +5,15 @@ var bullet_scene = preload("res://Scenes/bullet.tscn")
 @onready var shooty_part = $ShootPoint
 
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
+	# Make sure the character always faces right
+	look_at(global_position + Vector2(1, 0))
 
 	if Input.is_action_just_pressed("shoot"):
 		var bullet = bullet_scene.instantiate()
 		bullet.global_position = shooty_part.global_position
-		bullet.direction = (get_global_mouse_position() - global_position).normalized()
+		
+		# Always shoot to the right
+		bullet.direction = Vector2(1, 0)  # Rightward direction (positive x-axis)
 		$/root/Main.add_child(bullet)
 
 	for i in range(get_slide_collision_count()):
@@ -19,4 +22,3 @@ func _physics_process(delta):
 		if collision.get_collider().is_in_group("enemies") and not is_reloading:
 			is_reloading = true
 			get_tree().reload_current_scene()
-	
