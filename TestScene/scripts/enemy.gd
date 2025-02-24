bec164d1e1aee5e3dacad0340612cb300772fe28
logandav3707 <ldavis@ftstudent.org>
2025-02-24 13:13:52 -0500

    Made enemies shoot

--- TestScene/scripts/enemy.gd
+++ TestScene/scripts/enemy.gd
@@ -3,13 +3,15 @@ extends Area2D
 var speed := 200
 var time := 0.0
 var pivot_position := Vector2(0, 0)
-
+var bullet = preload("res://Enemies/enemy_bullet.tscn")
+var can_shoot = false
 # Random vertical movement variables
 var vertical_speed := 0.0  # Vertical speed component (randomly changes)
 var change_direction_time := 1.0  # Time in seconds before changing vertical direction
 var timer := 0.0  # Timer to track when to change vertical direction
 
 func _ready():
+	$Timer.start(2.0)
 	pivot_position = position
 	_set_random_vertical_direction()  # Initialize random vertical direction at the start
 	
@@ -22,7 +24,12 @@ func _physics_process(delta: float) -> void:
 	if timer >= change_direction_time:
 		_set_random_vertical_direction()
 		timer = 0.0
-
+	if can_shoot:
+		shoot()
+		
+		$Timer.start(randi() % 5 +1)
+		can_shoot = false
+		
 # Function to move the ship left while adding random vertical movement
 func move_ship(delta) -> void:
 	position.x -= speed * delta  # Always move to the left
@@ -41,3 +48,13 @@ func wavy_movement(delta, frequency: float, amplitude: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+func shoot():
+	var inst = bullet.instantiate()
+	
+	get_parent().add_child(inst)
+	
+	inst.global_position = $bullet_pos.global_position
+func _on_timer_timeout() -> void:
+	can_shoot = true
+	$Timer.stop()
