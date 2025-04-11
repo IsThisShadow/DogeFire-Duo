f2d66602945aa0ffa0aad87e041f1ab7f42ee54f
Gavy197 <ValakVox@gmail.com>
2025-04-11 12:15:17 -0400

    fixed enemy detection on weapon 1

--- scripts/Bullet1.gd
+++ scripts/Bullet1.gd
@@ -13,3 +13,9 @@ func _on_body_entered(body: Node):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemies"):
+		area.queue_free()  # Or call take_damage() if your enemies use health
+		queue_free()  # Destroy the bullet too
