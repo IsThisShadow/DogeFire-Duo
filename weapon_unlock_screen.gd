2fbd27587a9d43eb39714ee8042147ff07e813ef
Gavy197 <ValakVox@gmail.com>
2025-04-19 13:04:50 -0400

    No need for mouse
    Makes it so you can continue without having to use the mouse button, both players can press start to continue

--- weapon_unlock_screen.gd
+++ weapon_unlock_screen.gd
@@ -1,47 +1,60 @@
 extends Control
 
-@onready var continue_button = $UIBox/wrapperContinue/ContinueButton
-@onready var weapon_name_label = $UIBox/wrapperWeapon/weaponNameLabel
-@onready var weapon_icon = $UIBox/WeaponIcon
-@onready var next_level_label = $UIBox/wrapperNextLevel/nextLevelLabel
-@onready var equip_info_label = $UIBox/wrapperEquip/equipInfoLabel
-
+var p1_ready := false
+var p2_ready := false
+var countdown_started := false
 var is_two_player_mode := false
-var next_level_index := 0
+var next_level_index := 1  # Set this appropriately before this screen loads
 
 func _ready():
-	continue_button.pressed.connect(_on_continue_pressed)
+	# Hide "Ready" labels initially
+	$UIBox/ReadyWrapperP1/Player1ReadyLabel.visible = false
+	$UIBox/ReadyWrapperP2/Player2ReadyLabel.visible = false
+
+	# Set colors (red for P1, blue for P2)
+	$UIBox/ReadyWrapperP1/Player1ReadyLabel.add_theme_color_override("font_color", Color(1, 0.2, 0.2))
+	$UIBox/ReadyWrapperP2/Player2ReadyLabel.add_theme_color_override("font_color", Color(0.4, 0.6, 1.0))
+
+	# Start flashing prompt
+	$UIBox/wrapperContinue/CountdownLabel.text = "Press Start"
+	$UIBox/wrapperContinue/AnimationPlayer.play("FlashStart")
+
 	_show_weapon_info()
 
-func _show_weapon_info():
-	match next_level_index:
-		2:
-			#weapon_icon.texture = preload("res://gun (1).png")
-			weapon_name_label.text = "Weapon 3 - Laser Cannon"
-			next_level_label.text = "Next: Entering Level 2"
-			equip_info_label.text = "Press 'L2' to equip this weapon in-game"
-		3:
-			#weapon_icon.texture = preload("res://weapons/weapon4.png")
-			weapon_name_label.text = "Weapon 4 - Plasma Beam"
-			next_level_label.text = "Next: Entering Level 3"
-			equip_info_label.text = "Press 'R1' to equip this weapon in-game"
-		4:
-			# weapon_icon.texture = preload("res://weapons/weapon5.png")
-			weapon_name_label.text = "Weapon 5 - Omega Blaster"
-			next_level_label.text = "Next: Entering Level 4"
-			equip_info_label.text = "Press 'R2' to equip this weapon in-game"
-		5:
-			# weapon_icon.texture = null
-			weapon_name_label.text = "ALL Weapons Unlocked!"
-			next_level_label.text = "Next: Entering Final Level"
-			equip_info_label.text = "Use 1-5 keys to switch between all weapons"
-		_:
-			weapon_icon.texture = null
-			weapon_name_label.text = "New Weapon Unlocked!"
-			next_level_label.text = "Next Level Incoming..."
-			equip_info_label.text = ""
+func _process(delta):
+	if countdown_started:
+		return
 
-func _on_continue_pressed():
+	if Input.is_action_just_pressed("p1_start") and not p1_ready:
+		p1_ready = true
+		$UIBox/ReadyWrapperP1/Player1ReadyLabel.visible = true
+		$UIBox/ReadyWrapperP1/AnimationPlayer.play("ZoomIn")
+
+	if is_two_player_mode:
+		if Input.is_action_just_pressed("p2_start") and not p2_ready:
+			p2_ready = true
+			$UIBox/ReadyWrapperP2/Player2ReadyLabel.visible = true
+			$UIBox/ReadyWrapperP2/AnimationPlayer.play("ZoomIn")
+
+		if p1_ready and p2_ready:
+			start_countdown()
+	else:
+		if p1_ready:
+			start_countdown()
+
+func start_countdown():
+	countdown_started = true
+
+	$UIBox/wrapperContinue/AnimationPlayer.stop()
+	$UIBox/wrapperContinue/CountdownLabel.modulate.a = 1.0
+
+	await get_tree().create_timer(0.5).timeout
+	$UIBox/wrapperContinue/CountdownLabel.text = "READY!"
+
+	await get_tree().create_timer(0.5).timeout
+	load_next_level()
+
+func load_next_level():
 	var next_scene_path = "res://scripts/PlayerScripts/Levels/mainLvl_%d.tscn" % next_level_index
 	var next_scene = load(next_scene_path).instantiate()
 	next_scene.set_2_players(is_two_player_mode)
@@ -52,3 +65,30 @@ func _on_continue_pressed():
 
 	get_tree().get_root().add_child(next_scene)
 	get_tree().current_scene = next_scene
+
+func _show_weapon_info():
+	var weapon_name_label = $UIBox/wrapperWeapon/weaponNameLabel
+	var next_level_label = $UIBox/wrapperNextLevel/nextLevelLabel
+	var equip_info_label = $UIBox/wrapperEquip/equipInfoLabel
+
+	match next_level_index:
+		2:
+			weapon_name_label.text = "Weapon 3 - Laser Cannon"
+			next_level_label.text = "Next: Entering Level 2"
+			equip_info_label.text = "Press 'L2' to equip this weapon in-game"
+		3:
+			weapon_name_label.text = "Weapon 4 - Plasma Beam"
+			next_level_label.text = "Next: Entering Level 3"
+			equip_info_label.text = "Press 'R1' to equip this weapon in-game"
+		4:
+			weapon_name_label.text = "Weapon 5 - Omega Blaster"
+			next_level_label.text = "Next: Entering Level 4"
+			equip_info_label.text = "Press 'R2' to equip this weapon in-game"
+		5:
+			weapon_name_label.text = "ALL Weapons Unlocked!"
+			next_level_label.text = "Next: Entering Final Level"
+			equip_info_label.text = "Use 1-5 keys to switch between all weapons"
+		_:
+			weapon_name_label.text = "New Weapon Unlocked!"
+			next_level_label.text = "Next Level Incoming..."
+			equip_info_label.text = ""
