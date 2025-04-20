68b539e36ff8978794b59261876010c446396ab5
Gavy197 <ValakVox@gmail.com>
2025-04-20 11:47:33 -0400

    added quit button

--- you_died_screen.gd
+++ you_died_screen.gd
@@ -3,9 +3,15 @@ extends Control
 
 func _on_play_again_pressed() -> void:
 	get_tree().paused = false
-	await get_tree().process_frame  # allow pause state to clear
+	print("play again")
+	#await get_tree().process_frame  # allow pause state to clear
 	get_tree().change_scene_to_file("res://MainMenu.tscn")
 
 
 func _on_see_your_score_pressed() -> void:
 	print("Show score logic goes here.")
+
+
+
+func _on_quit_game_pressed() -> void:
+	get_tree().quit()
