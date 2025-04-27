09f6e06b79dca5a44a4e26fbf90b86b741e4b7fd
Gavy197 <ValakVox@gmail.com>
2025-04-27 14:16:31 -0400

    enemy 1 finished

--- enemies/enemy_1.gd
+++ enemies/enemy_1.gd
@@ -15,22 +15,30 @@ func _ready():
 func take_damage(amount):
 	if is_dead:
 		return
-		current_health -= amount
-		if current_health <= 0:
-			die()
+	current_health -= amount
+	print("Enemy health:", current_health) 
+	if current_health <= 0:
+		die()
 			
 			
 func die():
 	is_dead = true
 	
-	# Play death animation (if you have an AnimatedSprite2D)
-	if $AnimatedSprite2D:
-		$AnimatedSprite2D.play("death")
-	
-	# After animation is finished, remove the enemy
-	await $AnimatedSprite2D.animation_finished
+	# Hide the normal idle sprite
+	if $enemy_1_SupportShip:
+		$enemy_1_SupportShip.visible = false
+
+	# Show the DeathSprite
+	if $DeathSprite:
+		$DeathSprite.visible = true
+
+	# Play the death animation
+	if $AnimationPlayer:
+		$AnimationPlayer.play("death_enemy_1")
+
+	# Wait for death animation to finish, then remove enemy
+	await $AnimationPlayer.animation_finished
 	queue_free()
-	
 
 
 func _on_bullet_timer_timeout() -> void:
