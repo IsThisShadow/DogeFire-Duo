5beb147ab0d9b5decb6409dbdb2e9cf7afb4e841
Gavy197 <ValakVox@gmail.com>
2025-05-01 17:06:35 -0400

    i

--- UI/UI scripts/main_menu.gd
+++ UI/UI scripts/main_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/ButtonOneWrapper/OnePlayerButton.grab_focus()
-	$VBoxContainer/ButtonOneWrapper/OnePlayerButton.text = "1 Player"
-	$VBoxContainer/ButtonTwoWrapper/TwoPlayerButton.text = "2 Players"
+	$VBoxContainer/OnePlayerButton.grab_focus()
+	$VBoxContainer/OnePlayerButton.text = "1 Player"
+	$VBoxContainer/TwoPlayerButton.text = "2 Players"
 
-	$VBoxContainer/ButtonOneWrapper/OnePlayerButton.pressed.connect(_on_one_player)
-	$VBoxContainer/ButtonTwoWrapper/TwoPlayerButton.pressed.connect(_on_two_player)
+	$VBoxContainer/OnePlayerButton.pressed.connect(_on_one_player)
+	$VBoxContainer/TwoPlayerButton.pressed.connect(_on_two_player)
 
 func _on_one_player():
 	_load_ready_screen(false)
@@ -26,6 +26,18 @@ func _load_ready_screen(is_two_player: bool):
 	get_tree().current_scene = menu2_scene
 
 func _unhandled_input(event):
+	if event.is_action_pressed("p1_up") or event.is_action_pressed("p2_up"):
+		var focused = get_viewport().gui_get_focus_owner()
+		var neighbor = focused.get_focus_neighbor(0) # TOP
+		if neighbor:
+			neighbor.grab_focus()
+
+	elif event.is_action_pressed("p1_down") or event.is_action_pressed("p2_down"):
+		var focused = get_viewport().gui_get_focus_owner()
+		var neighbor = focused.get_focus_neighbor(1) # BOTTOM
+		if neighbor:
+			neighbor.grab_focus()
+
 	if event.is_action_pressed("p1_a") or event.is_action_pressed("p2_a"):
 		var focused = get_viewport().gui_get_focus_owner()
 		if focused and focused is Button:
